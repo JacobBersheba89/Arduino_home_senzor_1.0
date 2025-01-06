6711b7dee05f9ede9460f4b9583a0946ee535cf7
JacobP <jakubpawlas89@gmail.com>
2025-01-06 08:59:36 +0100

    Add files via upload

---
+++ 4_wifi_base_testing_connection.cpp
@@ -0,0 +1,12 @@
+#include <ESP8266WiFi.h>
+
+const char* ssid = "SSID";
+const char* password = "Heslo";
+
+void setup() {
+  Serial.begin(9600);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting..."); }
+  Serial.println("Yes, připojeno!"); }
