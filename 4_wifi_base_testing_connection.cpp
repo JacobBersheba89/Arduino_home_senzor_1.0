b45a0d959cf3671f2429e100960399c1457821cc
JacobP <jakubpawlas89@gmail.com>
2025-01-06 09:01:41 +0100

    Update 4_wifi_base_testing_connection.cpp

--- 4_wifi_base_testing_connection.cpp
+++ 4_wifi_base_testing_connection.cpp
@@ -7,6 +7,6 @@ void setup() {
   Serial.begin(9600);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+    delay(1000);
     Serial.println("Connecting..."); }
   Serial.println("Yes, připojeno!"); }
